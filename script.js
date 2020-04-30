e1b4a4a8263cddde37a49e700927a8c9c2697028
AmujoDotun <samuelamujo@gmail.com>
2020-04-30 15:03:23 +0100

    added checkbox to the list display

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ const form = document.getElementById('addForm');
 const todo = document.getElementById('button');
 
 todo.addEventListener('click', newTodo);
+list.addEventListener('click', checkedList);
 
 
 function newTodo(e) {
@@ -27,9 +28,24 @@ function newTodo(e) {
     li.className ="todoList";
 
     // add text node with the input value
-    let output = li.appendChild(document.createTextNode(todo));
-    list.appendChild(li);
-  console.log(li)
+    li.appendChild(document.createTextNode(todo));
+    
+    
   
+    // create delete button on each todo list
+
+    let TODO_CHECKBOX = document.createElement('input');
+
+    TODO_CHECKBOX.type ="checkbox";
+    // Add claas Name to button element
+    // TODO_CHECKBOX.className = "btn btn-danger btn-sm float-right delete"
+    
+    // TODO_CHECKBOX.appendChild(document.createTextNode());
+    li.appendChild(TODO_CHECKBOX);
+  
+    // Output the value to the DOM
+    list.appendChild(li);
+    console.log(TODO_CHECKBOX)
 
 }
+
