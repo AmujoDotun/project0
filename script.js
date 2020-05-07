f81a12952d198347cd9a09e06fff24e8042357a7
AmujoDotun <samuelamujo@gmail.com>
2020-05-07 04:02:28 +0100

    created countIt function

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ const classNames = {
 const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
-
+console.log(itemCountSpan);
 const form = document.getElementById('addForm');
 const todo = document.getElementById('button');
 
 todo.addEventListener('click', newTodo);
 list.addEventListener('click', checkedList);
-
+// list.addEventListener('click', countIt);
 
 function newTodo(e) {
   e.preventDefault();
@@ -28,10 +28,12 @@ function newTodo(e) {
     li.className ="todoList";
 
     // add text node with the input value
-    li.appendChild(document.createTextNode(todo));
+    // li.appendChild(document.createTextNode(todo));
+    li.appendChild(document.createTextNode(todo))
     
+      // itemCountSpan ++;
+      // console.log(itemCountSpan ++);
     
-  
     // create delete button on each todo list
 
     let TODO_CHECKBOX = document.createElement('input');
@@ -39,6 +41,7 @@ function newTodo(e) {
     TODO_CHECKBOX.type ="checkbox";
     // Add claas Name to button element
     TODO_CHECKBOX.className = "check done"
+   
     
     // TODO_CHECKBOX.appendChild(document.createTextNode());
     li.appendChild(TODO_CHECKBOX);
@@ -59,5 +62,14 @@ function checkedList(e)
       let li = e.target.parentElement;
       li.style.textDecoration = 'line-through';
     }
+  }
+}
+ let count =0;
+ console.log(count);
+function countIt(e)
+{
+  for( count =0; count<TODO_CHECKBOX.length; count++)
+  {
+    console.log(TODO_CHECKBOX.length);
   }
 }
